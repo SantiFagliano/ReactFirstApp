511a4d11bd10d9bef979f21d2ecbfb512e47da7d
SantiFagliano <santifagliano@live.com.ar>
2020-12-30 10:24:34 -0300

    limpiamos el codigo para empezar nuestra app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
+import React from "react";
 import "./App.css";
 
-function Helloword() {
-  return <div className="hello">Hello word</div>;
-}
-
-function App() {
-  return (
-    <div>
-      This is my component: <Helloword></Helloword>
-      <Helloword> </Helloword>
-      <Helloword> </Helloword>
-    </div>
-  );
-}
 
 export default App;
